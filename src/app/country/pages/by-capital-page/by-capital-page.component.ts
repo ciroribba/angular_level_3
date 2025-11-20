5b549b6f8c2b70074367d460790a2510f6cbe576
ciroribba <ciro.ribba@gmail.com>
2025-11-20 12:03:13 -0300

    Reactivity with resources

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, resource } from '@angular/core';
 import { SearchInput } from "../../components/search-input/search-input";
 import { CountryList } from "../../components/country-list/country-list";
 import { CountryService } from '../../services/country';
 import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 @Component({
   selector: 'by-capital-page',
   imports: [SearchInput, CountryList ],
@@ -10,28 +11,41 @@ import { Country } from '../../interfaces/country.interface';
 })
 export class ByCapitalPageComponent {
   countryService = inject(CountryService);
+  query = signal('');
 
-  isLoading = signal(false);
-  isError = signal<string|null>(null)
-  countries = signal<Country[]>([])
+  countryResource = resource({
+    params: () => ({query: this.query()}),
+    loader: async({ params }) => {
+      console.log({params});
+      if(!params.query) return [];
 
-  onSearchByCapital(query: string): void {
-    if(this.isLoading()) return;
-    this.isLoading.set(true);
-    this.isError.set(null);
+      return await firstValueFrom(
+        this.countryService.searchByCapital(params.query)
+      );
+    }
+  });
 
-     console.log({query});
-     this.countryService.searchByCapital(query).subscribe({
-      next: (countries) => {
-        this.countries.set(countries);
-      },
-      error: (error) => {
-        this.countries.set([]);
-        this.isError.set(error);
-      },
-      complete: () => {
-        this.isLoading.set(false);
-      }
-     });
-  }
+  // isLoading = signal(false);
+  // isError = signal<string|null>(null)
+  // countries = signal<Country[]>([])
+
+  // onSearchByCapital(query: string): void {
+  //   if(this.isLoading()) return;
+  //   this.isLoading.set(true);
+  //   this.isError.set(null);
+
+  //    console.log({query});
+  //    this.countryService.searchByCapital(query).subscribe({
+  //     next: (countries) => {
+  //       this.countries.set(countries);
+  //     },
+  //     error: (error) => {
+  //       this.countries.set([]);
+  //       this.isError.set(error);
+  //     },
+  //     complete: () => {
+  //       this.isLoading.set(false);
+  //     }
+  //    });
+  // }
  }
